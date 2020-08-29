a4b034a2c43c7c24a36e719ff8e8bee2c9712805
Karim Sharipov <karim0724@gmail.com>
2020-08-29 07:24:04 +0300

    Add auth view

--- src/main/java/com/netcracker/edu/distancestudyweb/controller/AuthController.java
+++ src/main/java/com/netcracker/edu/distancestudyweb/controller/AuthController.java
@@ -1,21 +1,55 @@
 package com.netcracker.edu.distancestudyweb.controller;
 
+import com.netcracker.edu.distancestudyweb.domain.ClientPrincipal;
+import com.netcracker.edu.distancestudyweb.dto.authentication.AuthenticationRequest;
 import com.netcracker.edu.distancestudyweb.service.AuthenticationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import static com.netcracker.edu.distancestudyweb.controller.ControllerUtils.SERVICE_ERROR_MESSAGE;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
-    private AuthenticationService authenticationService;
+    private AuthenticationService authService;
 
-    public AuthController(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
+    public AuthController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping
+    public String authenticate(AuthenticationRequest authRequest, Model model) {
+        String username = authRequest.getUsername();
+        String viewName = null;
+        try {
+            Authentication response = authService.authenticate(new UsernamePasswordAuthenticationToken(username, authRequest.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(response);
+        } catch (final BadCredentialsException ex) {
+            viewName = handleException(model,"Bad credentials");
+        } catch (Exception e) {
+            viewName = handleException(model, SERVICE_ERROR_MESSAGE);
+        }
+        return viewName;
     }
 
     @GetMapping
-    public String authenticate() {
+    public String authenticate(Model model) {
+        return "auth";
+    }
+
+    private String handleException(Model model, String errorMessage) {
+        model.addAttribute("message", errorMessage);
+        model.addAttribute("messageType","danger");
         return "auth";
     }
 
