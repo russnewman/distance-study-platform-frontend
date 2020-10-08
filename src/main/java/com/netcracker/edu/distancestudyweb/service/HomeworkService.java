21e22acc3e9656e28420b1a66ada974e0538f460
S80Sharipov-KR <sharipov@bpcbt.com>
2020-10-08 12:21:26 +0300

    Pagination for student homework

--- src/main/java/com/netcracker/edu/distancestudyweb/service/HomeworkService.java
+++ src/main/java/com/netcracker/edu/distancestudyweb/service/HomeworkService.java
@@ -1,13 +1,10 @@
 package com.netcracker.edu.distancestudyweb.service;
 
-import com.netcracker.edu.distancestudyweb.domain.StudentEvent;
 import com.netcracker.edu.distancestudyweb.dto.GetStudentEventsResponseDto;
 import com.netcracker.edu.distancestudyweb.dto.homework.AssignmentFormRequest;
 import com.netcracker.edu.distancestudyweb.dto.homework.EventFormRequest;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-
-import java.util.List;
 
 public interface HomeworkService {
     GetStudentEventsResponseDto getEvents(EventFormRequest formRequest);
